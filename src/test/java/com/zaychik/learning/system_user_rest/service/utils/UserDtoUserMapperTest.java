b17a2ec9f8e2952caad77f45d64c9d2ee2c448de
alex1 <alex1706.mephi@gmail.com>
2023-06-30 11:11:32 +0300

    Сделал тест UserDtoUserMapperTest

---
+++ src/test/java/com/zaychik/learning/system_user_rest/service/utils/UserDtoUserMapperTest.java
@@ -0,0 +1,48 @@
+package com.zaychik.learning.system_user_rest.service.utils;
+
+import com.zaychik.learning.system_user_rest.entity.Role;
+import com.zaychik.learning.system_user_rest.entity.User;
+import com.zaychik.learning.system_user_rest.entity.UserDto;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDtoUserMapperTest {
+
+    @Test
+    void mapToUserDto() {
+        User user = User.builder()
+                .id(1)
+                .email("alex@gmail.com")
+                .phone("8999777888123")
+                .name("Alex")
+                .password("qwert")
+                .role(Role.USER)
+                .build();
+        UserDto userDto = UserDtoUserMapper.INSTANCE.mapToUserDto(user);
+        assertEquals(userDto.getId(), user.getId());
+        assertEquals(userDto.getEmail(), user.getEmail());
+        assertEquals(userDto.getPhone(), user.getPhone());
+        assertEquals(userDto.getName(), user.getName());
+        assertEquals(userDto.getRole(), user.getRole());
+
+    }
+
+    @Test
+    void mapToUser() {
+        UserDto userDto = UserDto.builder()
+                .id(1)
+                .email("alex@gmail.com")
+                .phone("8999777888123")
+                .name("Alex")
+                .role(Role.USER)
+                .build();
+        User user = UserDtoUserMapper.INSTANCE.mapToUser(userDto);
+        assertEquals(userDto.getId(), user.getId());
+        assertEquals(userDto.getEmail(), user.getEmail());
+        assertEquals(userDto.getPhone(), user.getPhone());
+        assertEquals(userDto.getName(), user.getName());
+        assertEquals(userDto.getRole(), user.getRole());
+        assertNull(user.getPassword());
+    }
+}

e0140cb1d0095fd4ea196e6cc67a32e28ae4756a
alex1 <alex1706.mephi@gmail.com>
2023-07-05 23:34:54 +0300

    userDetailsService в отдельный сервис класс

--- src/main/java/com/zaychik/learning/system_user_rest/config/ApplicationConfiguration.java
+++ src/main/java/com/zaychik/learning/system_user_rest/config/ApplicationConfiguration.java
@@ -2,7 +2,9 @@ package com.zaychik.learning.system_user_rest.config;
 
 import com.zaychik.learning.system_user_rest.model.auth.UserAuth;
 import com.zaychik.learning.system_user_rest.repository.UserRepository;
+import com.zaychik.learning.system_user_rest.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,23 +20,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfiguration {
+    @Autowired
+    MyUserDetailsService userDetailsService;
 
-    private final UserRepository repository;
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return new UserAuth(repository.findByEmail(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found")));
-            }
-        };
-    }
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
---
+++ src/main/java/com/zaychik/learning/system_user_rest/service/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.zaychik.learning.system_user_rest.service;
+
+
+import com.zaychik.learning.system_user_rest.model.auth.UserAuth;
+import com.zaychik.learning.system_user_rest.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new UserAuth(repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found")));
+    }
+}

8a81e3adbc9d142dcd01de45ab6275b14b146dba
alex1 <alex1706.mephi@gmail.com>
2023-07-11 11:20:00 +0300

    Добавил тест на авторизацию

--- src/test/java/com/zaychik/learning/system_user_rest/controller/IntegrationAuthenticationControllerTest.java
+++ src/test/java/com/zaychik/learning/system_user_rest/controller/IntegrationAuthenticationControllerTest.java
@@ -44,7 +44,7 @@ public class IntegrationAuthenticationControllerTest extends AbstractIntegration
     }
     @Test
     @DisplayName("Регистрация пользователя, не уникальная почта - ошибка")
-    void register_ExistEmailError() throws Exception {
+    void register_ExistEmail_Error() throws Exception {
         RegisterRequest userNew = RegisterRequest.builder()
                 .email("user2@gmail.com")
                 .name("Alexander")
@@ -65,7 +65,7 @@ public class IntegrationAuthenticationControllerTest extends AbstractIntegration
 
     @Test
     @DisplayName("Регистрация пользователя, не указан пароль - ошибка")
-    void register_NoSetPasswordEmailError() throws Exception {
+    void register_NoSetPasswordEmail_Error() throws Exception {
         RegisterRequest userNew = RegisterRequest.builder()
                 .email("user3@gmail.com")
                 .name("Alexander")
@@ -81,4 +81,55 @@ public class IntegrationAuthenticationControllerTest extends AbstractIntegration
                 .andExpect(result -> assertTrue(result.getResolvedException().getMessage().contains("Enter a password")));
 
     }
+
+    @Test
+    @DisplayName("Авторизация, успешная")
+    void authenticate_Success() throws Exception {
+        AuthenticationRequest user = AuthenticationRequest.builder()
+                .email("user@gmail.com")
+                .password("1234")
+                .build();
+        AuthenticationResponce response = performAuthentication(user);
+
+        Assertions.assertFalse(response.getToken().equals(""));
+    }
+
+    @Test
+    @DisplayName("Авторизация пользователя, не правильный пароль - ошибка")
+    void authenticate_NoCorrectPassword_Error() throws Exception {
+        AuthenticationRequest user = AuthenticationRequest.builder()
+                .email("user@gmail.com")
+                .password("12345")
+                .build();
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/authentication")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("Авторизация пользователя, нет такого пользователя - ошибка")
+    void authenticate_NoExistUser_Error() throws Exception {
+        AuthenticationRequest user10 = AuthenticationRequest.builder()
+                .email("user10@gmail.com")
+                .password("1234")
+                .build();
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/authentication")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(user10)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+
+
+
+
 }

af8b7d3b7e9de385673b439bb547706f881b3ff5
alex1 <alex1706.mephi@gmail.com>
2023-08-06 09:46:18 +0300

    Убрал аннотацию @ExtendWith(MockitoExtension.class) версия 2

--- src/test/java/com/zaychik/learning/system_user_rest/service/LogElementServiceTest.java
+++ src/test/java/com/zaychik/learning/system_user_rest/service/LogElementServiceTest.java
@@ -2,34 +2,28 @@ package com.zaychik.learning.system_user_rest.service;
 
 import com.zaychik.learning.system_user_rest.model.LogElement;
 import com.zaychik.learning.system_user_rest.repository.LogElementRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
 
 class LogElementServiceTest {
-    AutoCloseable openMocks;
-    @Mock
     private LogElementRepository logElementRepository;
-    @InjectMocks
     private LogElementService service;
     private static final String USER_EMAIL = "admin@gmail.com";
     @BeforeEach
     void setUp() {
-        openMocks = MockitoAnnotations.openMocks(this);
+        logElementRepository = mock(LogElementRepository.class);
+        service = new LogElementService(logElementRepository);
     }
-
-        @Test
+    @Test
     void readAllbyUserEmail() {
         List<LogElement> logElementList = new LinkedList<LogElement>();
         LogElement logElement1 = LogElement.builder()
@@ -54,9 +48,5 @@ class LogElementServiceTest {
                 () -> Mockito.verify(logElementRepository, Mockito.times(1)).findAllByUserEmail(USER_EMAIL)
         );
     }
-    @AfterEach
-    void tearDown() throws Exception {
-        // my tear down code...
-        openMocks.close();
-    }
+
 }
